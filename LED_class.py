912b2d57cd2930331f99f1d5ab98718b8f1f6640
quahog78723 <33141192+quahog78723@users.noreply.github.com>
2018-04-01 16:57:39 -0500

    Update LED_class.py

--- LED_class.py
+++ LED_class.py
@@ -5,11 +5,11 @@
 # Planned enhancements: 
 # 		- Add PWM capabilities and appropriate methods
 
-import RPi.GPIO as GPIO						# Use RPi.GPIO module and reference as GPIO
+import RPi.GPIO as GPIO			# Use RPi.GPIO module and reference as GPIO
 
-class LED:                                  # Define LED class	
-	def __init__(self,pin):                 # Instance constructor, takes a PIN ID as a parameter
-                                            # Assumes calling program has set GPIO mode (BOARD or BCM)
+class LED:                              # Define LED class	
+	def __init__(self,pin):         # Instance constructor, takes a PIN ID as a parameter
+                                        # Assumes calling program has set GPIO mode (BOARD or BCM)
 		self.__pin__ = pin                  # Store pin ID as hidden attribute
 		GPIO.setup(self.__pin__,GPIO.OUT)   # Set pin as output
 		GPIO.output(self.__pin__, False)    # Default...LED is off (PIN is low)
