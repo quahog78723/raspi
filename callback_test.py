45fa364d90bb470412442772456110acf96f0908
quahog78723 <33141192+quahog78723@users.noreply.github.com>
2018-04-01 15:47:42 -0500

    Update callback_test.py

--- callback_test.py
+++ callback_test.py
@@ -1,22 +1,26 @@
-import RPi.GPIO as GPIO
+# Test script for showing how a call back function can be called based on a GPIO pin event
+# Also, shows how to block execution until an event occurs.
 
+import RPi.GPIO as GPIO                     # Import RPI.GPIO module
 
-def gpio_callback(channel):
-    print("Call back channel", channel)
+INPUT_PIN = 15                              # Set a constant to the pin to be used in this test
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(15,GPIO.IN)
+def gpio_callback(channel):                 # Define function (this function name will be passed when setting up call back)
+                                            # On callback, the channel (pin # ) causing the call back will be passed as a param
+    print("Call back channel", channel)     # Simple print statement showing the channel/pin
 
-#GPIO.add_event_detect(15,GPIO.FALLING,callback=gpio_callback)
+GPIO.setmode(GPIO.BOARD)                    # Set mode to use the BOARD numbering scheme 
+GPIO.setup(INPUT_PIN,GPIO.IN)               # Set the PIN as an inout pin
 
+# Uncomment the following to test the call back capability
+# GPIO.add_event_detect(INPUT_PIN,GPIO.FALLING,callback=gpio_callback)
+
+# The followingwas added as a test for the 'blocking' function of wait_for_edge. This call
+# will pause the mainline of the program until th event is detected and then the main line will continue.
 
 cont = True
 while cont:
-    #if GPIO.event_detected(15):
-    print("Starting wait")
-    GPIO.wait_for_edge(15,GPIO.BOTH)
+    print("Starting wait")                  
+    GPIO.wait_for_edge(INPUT_PIN,GPIO.BOTH)        # Call that blocks execution until an edge (falling or rising) is detected.
     print("Post wait")
-        
-        
-
-        
+       
