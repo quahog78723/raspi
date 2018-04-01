fba390fcc9ab70b61a86d49aad5ef122ed2af7a8
quahog78723 <33141192+quahog78723@users.noreply.github.com>
2018-04-01 17:03:50 -0500

    Update led-selector.py

--- led-selector.py
+++ led-selector.py
@@ -1,39 +1,39 @@
 # A simple GUI based on Tkinter used to turn LEDs on or off.
 
-import tkinter              # Import tkinter for GUI stuff
-import RPi.GPIO as GPIO     # Import RPi.GPIO as GPIO
-import LED_class            # Import our LED_class fiel that includes our LED class
+import tkinter				# Import tkinter for GUI stuff
+import RPi.GPIO as GPIO		# Import RPi.GPIO as GPIO
+import LED_class			# Import our LED_class fiel that includes our LED class
 
-BCM_LED8 = 5    # Output GPIO for LED8
-BCM_LED4 = 6    # Output GPIO for LED4
-BCM_LED2 = 13   # Output GPIO for LED2
-BCM_LED1 = 19   # Output GPIO for LED1
+BCM_LED8 = 5   				# Output GPIO for LED8
+BCM_LED4 = 6    			# Output GPIO for LED4
+BCM_LED2 = 13   			# Output GPIO for LED2
+BCM_LED1 = 19   			# Output GPIO for LED1
 
 def showChoice():           # Functiom to be called whenever the user selects a new LED to be turned on
-    print("Button: ",v.get())   # Shows a text messge in console containing the button that has been clicked
-    led = v.get()           # Get the LED nuber associated with the radio button clicked
-    if led == 1:            # If 1, turn on 1 and turn off others
-        led1.set_on()
-        led2.set_off()
-        led4.set_off()
-        led8.set_off()
-    elif led == 2:          # IF 2, turn on 2 and turn off others
-        led1.set_off()
-        led2.set_on()
-        led4.set_off()
-        led8.set_off()
-    elif led == 4:          # If 4, turn on 4 and turn off others
-        led1.set_off()
-        led2.set_off()
-        led4.set_on()
-        led8.set_off()
-    elif led == 8:          # If 8, turn on 8 and turn off others
-        led1.set_off()
-        led2.set_off()
-        led4.set_off()
-        led8.set_on()
-    else:                   # Should never get to this point
-        pass
+	print("Button: ",v.get())   # Shows a text messge in console containing the button that has been clicked
+	led = v.get()           # Get the LED nuber associated with the radio button clicked
+	if led == 1:            # If 1, turn on 1 and turn off others
+		led1.set_on()
+		led2.set_off()
+		led4.set_off()
+		led8.set_off()
+	elif led == 2:          # IF 2, turn on 2 and turn off others
+		led1.set_off()	
+		led2.set_on()
+		led4.set_off()
+		led8.set_off()
+	elif led == 4:          # If 4, turn on 4 and turn off others
+		led1.set_off()
+		led2.set_off()
+		led4.set_on()
+		led8.set_off()
+	elif led == 8:          # If 8, turn on 8 and turn off others
+		led1.set_off()
+		led2.set_off()
+		led4.set_off()
+		led8.set_on()
+	else:                   # Should never get to this point
+		pass
         
 # Main line of program
 
