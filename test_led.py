50af7efcac0c5e7fd6fd1ef50849c042e590a07f
quahog78723 <33141192+quahog78723@users.noreply.github.com>
2018-04-01 16:07:37 -0500

    Update test_led.py

--- test_led.py
+++ test_led.py
@@ -1,3 +1,5 @@
+# Simple test script for the initial testing of the LED class and turning an LED on or off
+
 import RPi.GPIO as GPIO
 import LED_class
 
@@ -5,11 +7,11 @@ GPIO.setmode(GPIO.BCM)
 
 led = LED_class.LED(5)
 
-input()
+input()   # Wait for user to hit the enter key
 
 led.set_on()
 
-input()
+input()   # Wait for user to hit the enter key
 led.set_off()
 
 GPIO.cleanup()
