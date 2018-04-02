ff95b507db7c5f7a3bfd46e113bfb88a0a3f45f9
quahog78723 <33141192+quahog78723@users.noreply.github.com>
2018-04-01 22:18:27 -0500

    Update LED_class.py

--- LED_class.py
+++ LED_class.py
@@ -10,16 +10,19 @@ import RPi.GPIO as GPIO			# Use RPi.GPIO module and reference as GPIO
 class LED:                              # Define LED class	
 	def __init__(self,pin):         # Instance constructor, takes a PIN ID as a parameter
                                         # Assumes calling program has set GPIO mode (BOARD or BCM)
-		self.__pin__ = pin                  # Store pin ID as hidden attribute
-		GPIO.setup(self.__pin__,GPIO.OUT)   # Set pin as output
-		GPIO.output(self.__pin__, False)    # Default...LED is off (PIN is low)
+		self.port = pin                  # Store pin ID as hidden attribute
+		GPIO.setup(self.port,GPIO.OUT)   # Set pin as output
+		GPIO.output(self.port, False)    # Default...LED is off (PIN is low)
 
 	def set_on(self):                       # set_on method - Sets pin high (Turns on LED)
-		GPIO.output(self.__pin__,True)
+		GPIO.output(self.port,True)
 
 	def set_off(self):                      # set_off method - Sets pin low (Turns off LED)
-		GPIO.output(self.__pin__, False)
-		
-	def set(self,__pin__,on_off):		# Pass boolean vaue to indicate whether to turn on (True) or off (False)
-		GPIO.output(self.__pin__,on_off)
+		GPIO.output(self.port, False)
+
+	def set(self,on_off):		# Pass boolean vaue to indicate whether to turn on (True) or off (False)
+		GPIO.output(self.port,on_off)
+
+	def get_port(self):
+                return (self.port)
 # End of LED class
