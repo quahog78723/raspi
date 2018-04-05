d8d748eebe5d401335eb8460fc687b1517fd94f2
Bart <33141192+quahog78723@users.noreply.github.com>
2018-04-05 16:31:18 -0500

    Updatd by Bart
    Added more comments.
    Changed the port variable to __pin (double underscore hides the attribute)

--- LED_class.py
+++ LED_class.py
@@ -1,6 +1,10 @@
-# LEDclass provides a simple way to interface with individual LEDs
 # Simple class for creating LED instances. The LEDs associated with these 
-# instances can be set_on or set_off.
+# instances can be set_on or set_off. 
+# The constructor method takes a pin number as a parameter, identifying the pin that the 
+# LED is associated with.
+# There is also a generic set method where
+# the paramter can be true or False for turning the LED on or off.
+# get_pin method returns the pin associated wit the LED.
 
 # Planned enhancements: 
 # 		- Add PWM capabilities and appropriate methods
@@ -10,19 +14,19 @@ import RPi.GPIO as GPIO			# Use RPi.GPIO module and reference as GPIO
 class LED:                              # Define LED class	
 	def __init__(self,pin):         # Instance constructor, takes a PIN ID as a parameter
                                         # Assumes calling program has set GPIO mode (BOARD or BCM)
-		self.port = pin                  # Store pin ID as hidden attribute
-		GPIO.setup(self.port,GPIO.OUT)   # Set pin as output
-		GPIO.output(self.port, False)    # Default...LED is off (PIN is low)
+		self.__pin = pin                  # Store pin ID as hidden attribute
+		GPIO.setup(self.__pin,GPIO.OUT)   # Set pin as output
+		GPIO.output(self.__pin, False)    # Default...LED is off (PIN is low)
 
 	def set_on(self):                       # set_on method - Sets pin high (Turns on LED)
-		GPIO.output(self.port,True)
+		GPIO.output(self.__pin,True)
 
 	def set_off(self):                      # set_off method - Sets pin low (Turns off LED)
-		GPIO.output(self.port, False)
+		GPIO.output(self.__pin, False)
 
 	def set(self,on_off):		# Pass boolean vaue to indicate whether to turn on (True) or off (False)
-		GPIO.output(self.port,on_off)
+		GPIO.output(self.__pin,on_off)
 
-	def get_port(self):
-                return (self.port)
+	def get_pin(self):
+                return (self.__pin)
 # End of LED class
