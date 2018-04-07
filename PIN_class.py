e6e5573036355d78d541c2566b65cae7e19863cd
Bart <33141192+quahog78723@users.noreply.github.com>
2018-04-06 21:51:49 -0500

    Add files via upload

--- PIN_class.py
+++ PIN_class.py
@@ -1,82 +1,63 @@
 # The PIN class provides a simple way to interface with individual GPIO PINs and will maintain
 # various attributes associated with the PIN.
 
-#import RPi.GPIO as GPIO					# Use RPi.GPIO module and reference as GPIO
+#import RPi.GPIO as GPIO                                        # Use RPi.GPIO module and reference as GPIO
 
-class PIN:								# Define PIN class	
-	UNDEFINED 	= 0
-	INPUT		= 1
-	OUTPUT		= 2
-	LOW		= 1
-	HIGH		= 2
-	
-	
-	
-	def __init__(self,pin):         	# Instance constructor, takes a PIN ID as a parameter
-                self.UNDEFINED  = 0
-                self.INPUT      = 1
-                self.OUTPUT     = 2
-                self.LOW        = 1
-                self.HIGH       = 2
-                self.__id 	= str(pin)
-                self.__mode     = self.UNDEFINED
-                self.__status	= self.UNDEFINED
-                self.__gui_obj  = ""
+class PIN:                                                              # Define PIN class
+        UNDEFINED       = -1
+        INPUT           = 1
+        OUTPUT          = 0
+        LOW             = 0
+        HIGH            = 1
 
-	def setup(self,mode):
-		self.__mode = mode
-		if mode == self.INPUT:
-                    pass
-		    #GPIO.setup(self.port,GPIO.IN)
-		elif mode == self.OUTPUT:
-                    pass
-		    #GPIO.setup(self.port,GPIO.OUT)
-	
-	def set(self,on_off):		# Pass boolean vaue to indicate whether to turn on (True) or off (False)
-		self.__status = on_off	
-		#GPIO.output(self.port,on_off)
+        def __init__(self,pin):
+		self.UNDEFINED = -1
+		self.INPUT = 1
+		self.OUTPUT = 0
+		self.LOW = 0
+		self.HIGH = 1
+		self.__id = str(pin)
+		self.__direction = self.UNDEFINED
+		self.__status = self.UNDEFINED
 
-	def __str__(self):
-		if self.__gui_obj=="":
-			gui_txt = "{null}"
+	def set_direction(self,dir):
+		if dir == self.INPUT:
+			self.__direction = dir
+			#GPIO.setup(self.port,GPIO.IN)
+		elif dir == self.OUTPUT:
+			self.__direction = dir
+			#GPIO.setup(self.port,GPIO.OUT)
 		else:
-			gui_txt = str(self.__gui.obj)
-		return "PIN ID: " + str(self.__id) + "\tMode: " + str(self.__mode) +  "\tStatus: " + str(self.__status) +"\tGUI Obj: " + gui_txt
+			print("Error ...shold not get here.")
 
-	def set_rb1(self,rb):
-		self.__rb1 = rb
-		
-	def set_rb2(self,rb):
-		self.__rb2 = rb
-		
-	def set_var(self,var):
-		self.__var = var
+        def set_output(self,b_on):              # Pass boolean vaue to indicate whether to turn on (True) or off (False)
+                self.__status = b_on
+                #GPIO.output(self.port,on_off)
 
-	def get_port(self):
-		return (self.__id)
+        def __str__(self):
+                return "PIN ID: " + str(self.__id) + "\tDirection: " + str(self.__direction) +  "\tStatus: " + str(self.__status)
 
-	def get_mode(self):
-		return (self.__mode)
+        def set_var(self,var):
+                self.__var = var
 
-	def get_status(self):
-		returrn (self.__status)
+        def get_id(self):
+                return (self.__id)
 
-	def get_gui_obj(self):
-		return (self.__gui_obj)
+        def get_status(self):
+                returrn (self.__status)
 
-	def pin_selected(self):
-		selected_mode = self.__var.get()
-		if selected_mode == self.get_mode():
-			print("Button already selected")
-		else:
-			self.setup(selected_mode)
-			if selected_mode == self.UNDEFINED:
-				sel_text = "RESET"
-			elif selected_mode == self.INPUT:
-				sel_text = "INPUT"
-			elif selected_mode == self.OUTPUT:
-				sel_text = "OUTPUT"
-			else:
-				print("Invalid ... selected mode = ",selected_mode)
-			print(" Pin: ",self.get_port(),"set to", sel_text)
+        def pin_selected(self):
+                selected_mode = self.__var.get()
+                if selected_mode == self.get_direction():
+                        print("Button already selected")
+                else:
+                        self.set_direction(selected_mode)
+
+                        if selected_mode == self.INPUT:
+                                sel_text = "INPUT"
+                        elif selected_mode == self.OUTPUT:
+                                sel_text = "OUTPUT"
+                        else:
+                                print("Invalid ... selected direction = ",selected_mode)
+                        print(" Pin: ",self.get_id(),"set to", sel_text)
 # End of PIN class
--- gui-dev-1.py
+++ gui-dev-1.py
@@ -2,49 +2,48 @@ import tkinter as tk
 from tkinter import *
 import PIN_class as PIN
 
-NUM_PINS    = 10
+# --------------
+# Constant definitions
+# --------------
+
+NUM_PINS        = 10    # Number of pins to be shown in dashboard
+P_NM_COL_W      = 10    # Pin name column width
+P_RB_COL_W      = 6     # Radiobutton column width
+COL_PAD         = 5    # Column padding
+P_NM_COL        = 0     # Column number for pin name 
+INPUT_COL       = 1     # Column number for input RB
+OUTPUT_COL      = 2     # Column number for output RB
 
 # --------------
 # Main line of program
 #---------------
+pins=[]                 # Create an empty list of pins
 
 root = tk.Tk()          # Initialize gui
 root.title("GPIO Dashboard")
 
-pins=[]               # Create an empty list of pins
-#frames = []             # Create an empty list of individual gui frames
-
-fhdr = tk.Frame(root,relief=RIDGE)  # Create header frame
-fhdr.grid(row=0,column=0)           # add header frame to grid
-
-# Add 4 labels to header frame
-tk.Label(fhdr,text = "PIN Name", relief=RIDGE,padx=10,width=10).grid(row=0,column=0)
-tk.Label(fhdr,text = "RESET", relief=RIDGE,padx=10,width=10).grid(row=0,column=1)
-tk.Label(fhdr,text = "INPUT", relief=RIDGE,padx=10,width=10).grid(row=0,column=2)
-tk.Label(fhdr,text = "OUTPUT", relief=RIDGE,padx=10,width=10).grid(row=0,column=3)
+# Add labels to header frame
+tk.Label(root,text = "PIN Name", fg='blue',padx=COL_PAD,width=P_NM_COL_W).grid(row=0,column=P_NM_COL)
+tk.Label(root,text = "INPUT",    fg='blue',padx=COL_PAD,width=P_RB_COL_W).grid(row=0,column=INPUT_COL)
+tk.Label(root,text = "OUTPUT",   fg='blue',padx=COL_PAD,width=P_RB_COL_W).grid(row=0,column=OUTPUT_COL)
          
 # for each pin (start numbering at 1)
 for i in range(1,NUM_PINS+1):
-	f = tk.Frame(root,relief=RIDGE) # Create a new frame for each pin
-	f.grid(row=i,column=0)
-
-	pin = PIN.PIN(i)
-	v = tk.IntVar()           # Create pin specific string var
-	v.set(pin.UNDEFINED)         # Set value to UNDEFINED
-	pin.set_var(v)
+	pin = PIN.PIN(i)                # Create pin object
+	v = tk.IntVar()                 # Create pin specific var to hold value of rb clicked
+	v.set(pin.UNDEFINED)            # Set default value of RBs to value to UNDEFINED
+	pin.set_var(v)                  # Set the initial mode of pin to UNDEFINED
 
 	# Add lbl(pin name) and three radio buttons to each frame
-	lbl = tk.Label(f,padx=10,width=10,text = "PIN"+str(i), relief=RIDGE)
-	but_reset   = tk.Radiobutton(f,padx=10,width=10,variable=v,value=pin.UNDEFINED,command=pin.pin_selected)
-	but_input   = tk.Radiobutton(f,padx=10,width=10,variable=v,value=pin.INPUT,command=pin.pin_selected)
-	but_output  = tk.Radiobutton(f,padx=10,width=10,variable=v,value=pin.OUTPUT,command=pin.pin_selected)
+	lbl = tk.Label(root,padx=COL_PAD,width=P_NM_COL_W,text = "PIN-"+str(i))
+	but_input   = tk.Radiobutton(root,padx=COL_PAD,width=P_RB_COL_W,variable=v,value=pin.INPUT,command=pin.pin_selected)
+	but_output  = tk.Radiobutton(root,padx=COL_PAD,width=P_RB_COL_W,variable=v,value=pin.OUTPUT,command=pin.pin_selected)
 
-	lbl.grid(row=i,column=0)
-	but_reset.grid(row=i,column=1)
-	but_input.grid(row=i,column=2)
-	but_output.grid(row=i,column=3)
+	lbl.grid(row=i,column=P_NM_COL)
+	but_input.grid(row=i,column=INPUT_COL)
+	but_output.grid(row=i,column=OUTPUT_COL)
 	
-	pins.append([lbl,pin])  # Is this needed?
+	pins.append([lbl,pin])  # Is this needed/useful?
 	
 root.mainloop()
 
