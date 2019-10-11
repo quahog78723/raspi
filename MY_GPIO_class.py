6314aee5d2c596200670727fae8108c48d5b5b45
Bart <33141192+quahog78723@users.noreply.github.com>
2019-10-11 10:31:36 -0500

    Update MY_GPIO_class.py

--- MY_GPIO_class.py
+++ MY_GPIO_class.py
@@ -1,3 +1,11 @@
+# MY_GPIO_class defines myGPIO class and provides 
+# initialization logic and clean up logic.
+#
+# Written by Bart Jacob
+#
+# Planned enhancements:
+# Provide initialization parameters (such as BOARD vs BCM etc)
+
 import RPi.GPIO as GPIO
 
 class myGPIO:
@@ -8,8 +16,7 @@ class myGPIO:
         GPIO.setwarnings(False)
         print("GPIO warnings disabled")
 
-
-    
+   
     def cleanup(self):
         print("Exiting ...resetting GPIO.")
         GPIO.cleanup()
