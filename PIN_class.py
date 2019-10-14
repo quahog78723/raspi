9efb25814747c39946253e2d5cd782b501dd495a
Bart <33141192+quahog78723@users.noreply.github.com>
2019-10-14 21:19:36 +0000

    Add files via upload

--- PIN_class.py
+++ PIN_class.py
@@ -3,120 +3,83 @@
 
 import RPi.GPIO as GPIO                                        # Use RPi.GPIO module and reference as GPIO
 
-class PIN:                                                              # Define PIN class
+class PIN:              # Define PIN class
+        # --------------
+        # class variables (constants)
+        # --------------
+        NO_GPIO = False         # Used for debugging GUI without calling GPIO
+
+        UNDEFINED = -1     # Used for direction or high/low status
+        INPUT = 1          # Input direction indicator
+        OUTPUT = 0         # Output direction indicator
+        INPUT_TXT = "IN"
+        OUTPUT_TXT = "OUT"
+        LOW = 0            # Pin low indicator
+        HIGH = 1           # Pin high indicator
+        PWM_ON = 1
+        PWM_OFF = 0
+        ED_ON = 1
+        ED_OFF = 0
+        
         # --------------
         # Constructor method...called when instance is created
         # --------------        
         def __init__(self,pin):         # pin is an integer identifying the pin number (assume BOARD mode numbering
-                self.UNDEFINED = -1     # Used for direction or high/low status
-                self.INPUT = 1          # Input direction indicator
-                self.OUTPUT = 0         # Output direction indicator
-                self.INPUT_TXT = "IN"
-                self.OUTPUT_TXT = "OUT"
-                self.LOW = 0            # Pin low indicator
-                self.HIGH = 1           # Pin high indicator
-                self.PWM_ON = 1
-                self.PWM_OFF = 0
 
-              # Instance variables  
-                self.__id = pin         # Set ID to pin number
-                self.__direction = self.INPUT_TXT    # Variable indicating current direction
-                self.__status = 0       # Variable indicating high/low pin status
-                self.__pwm = 0          # Variable to point to pwm object if initialized
-                self.__event_det = 0    # Indicates if event detection is on or off for this pin
+              # Instance variables
+              
+                self.__id = pin                         # Set ID to pin number
+                self.__direction = PIN.INPUT_TXT        # Variable indicating current direction
+                self.__status   = PIN.LOW               # Variable indicating high/low pin status       
+                self.__pwm      = PIN.PWM_OFF           # Variable to point to pwm object if initialized
+                self.__event_det = PIN.ED_OFF           # Indicates if event detection is on or off for this pin
 
                 # Variables used to access GUI controls
-#                self.__RB_inp = self.UNDEFINED
-#                self.__RB_out = self.UNDEFINED
-                self.__OM_DIR = self.UNDEFINED
-                self.__OH_cBox = self.UNDEFINED  
-                self.__SB_freq = self.UNDEFINED
-                self.__SB_dcyc = self.UNDEFINED
+                
+                self.__OM_dir   = PIN.UNDEFINED
+                self.__CB_outH  = PIN.UNDEFINED  
+                self.__SB_freq  = PIN.UNDEFINED
+                self.__SB_dcyc  = PIN.UNDEFINED
 
-                # Variables used by GUI controls to pass 
-                self.__DIR_var = self.INPUT
-                self.__OH_var = self.LOW        # Initialize variable that will contain variable for output high status
-                self.__PWM_var = self.PWM_OFF
-                self.__FREQ_var = 0
-                self.__DCYC_var = 0
-                self.__EDGE_var = 0
-                self.__BOUNCE_var = 0
+                # Variables used by GUI controls to pass new value of changed widget
+                
+                self.__DIR_var  = PIN.UNDEFINED
+                self.__OH_var   = PIN.UNDEFINED        
+                self.__PWM_var  = PIN.UNDEFINED
+                self.__FREQ_var = PIN.UNDEFINED             
+                self.__DCYC_var = PIN.UNDEFINED             
+                self.__EDGE_var = PIN.UNDEFINED             
+                self.__BOUNCE_var = PIN.UNDEFINED           
 
                 # Reset output
                 
-                self.set_direction(self.OUTPUT_TXT) #self.OUTPUT) # Initialize direction to output
-                self.set_output(False)          # Set output to False (low) - Needed because of 'sticky' behavior of RPi
-                self.set_direction(self.INPUT_TXT) #self.INPUT)  # Set direction to input
+                self.set_direction(PIN.OUTPUT_TXT)      # Initialize direction to output
+                self.set_output(False)                  # Set output to False (low) - Needed because of 'sticky' behavior of RPi
+#                self.set_direction(PIN.INPUT_TXT)       # Set direction to input
 
-                GPIO.setwarnings(False)
+                if not PIN.NO_GPIO:
+                        GPIO.setwarnings(False)
 
         # --------------
-        # Method to return pin id
+        # Methods to return instance variables
         # --------------
-        def get_id(self):
+        def get_id(self):               # PIN id
                 return self.__id
 
-        # --------------
-        # Method to get current direction of pin
-        # --------------
-        def get_direction(self):
+        def get_direction(self):        # PIN dirrection
                 return self.__direction
 
-        # --------------
-        # Method to return status (high/low)
-        # --------------
-        def get_status(self):
+        def get_status(self):           # Output high/low
                 return (self.__status)
 
-        # --------------
-        # Method to return pwm object
-        # --------------
-        def get_pwm(self):
+        def get_pwm(self):              # PWM off/on
                 return (self.__pwm)
 
-        # --------------
-        # Method to return state of event detection
-        # --------------
-
-        def get_event_det(self):
+        def get_event_det(self):        # Event detection off/on
                 return self.__event_det
 
         # --------------
-        # Method to add event detection for a pin
-        # --------------
-        def set_eventDet(self, edge):
-                if (self.__id != 19) and (self.__id != 21) :  # pin 19 and 21 seem to constantly switch state
-                        GPIO.add_event_detect(self.__id,GPIO.BOTH,callback=self.callback_pin_state_change,bouncetime=200)
-                        self.__event_det = 1
-        # --------------
-        # Method to set direction (input or output) of pin
-        # --------------
-        def set_direction(self,dir):
-                print("In set_direction")
-                if dir == self.INPUT_TXT: #self.INPUT:          # If input
-                        self.__direction = dir                  # Set __direction variable
-                        GPIO.setup(self.__id,GPIO.IN)           # Set pin for input
-                        if self.__OH_cBox != self.UNDEFINED:    # If gui chkbox exists, disable chkbox
-                                self.__OH_cBox.config(state="disabled")
-                        self.set_eventDet(GPIO.BOTH)
-                elif dir == self.OUTPUT_TXT: #self.OUTPUT:      # if output
-                        self.__direction = dir #dir             # Set __direction variable
-                        try:
-                                if self.__event_det == 1:
-                                        self.__event_det = 0
-                                        GPIO.remove_event_detect(self.__id)        
-                        except Exception as err:
-                                print("***",end=" ")
-                                print(err)
-                        GPIO.setup(self.__id,GPIO.OUT)          # Set pin for output
-                        if self.__OH_cBox !=self.UNDEFINED:     # If gui chkbox exists... 
-                                self.__OH_cBox.config(state="normal") #  enable chkbox
-                else:
-                        print("Error ...shold not get here.")
-
-
-        # --------------
-        # Method to set tkinter variable associated with direction radio buttons
+        # Methods to set/get tkinter variable associated with direction option menu
         # --------------
         def set_DIR_var(self,var):
                 self.__DIR_var = var
@@ -125,7 +88,7 @@ class PIN:                                                              # Define
                 return self.__DIR_var
 
         # --------------
-        # Method to set tkinter variable associated with output high chkbox
+        # Methods to set/get tkinter variable associated with output high chkbox
         # --------------
         def set_OUT_var(self,var):
                 self.__OH_var = var
@@ -134,15 +97,16 @@ class PIN:                                                              # Define
                 return self.__OH_var
         
         # --------------
-        # Method to set tkinter variable associated with PWM high chkbox
+        # Methods to set/get tkinter variable associated with PWM high chkbox
         # --------------
         def set_PWM_var(self,var):
                 self.__PWM_var = var
 
         def get_PWM_var(self):
                 return self.__PWM_var
+        
         # --------------
-        # Method to set tkinter variable associated with Frequency spinbox
+        # Methods to set/get tkinter variable associated with Frequency spinbox
         # --------------
         def set_FREQ_var(self,var):
                 self.__FREQ_var = var
@@ -151,7 +115,7 @@ class PIN:                                                              # Define
                 return self.__FREQ_var
                 
         # --------------
-        # Method to set tkinter variable associated with DutyCycle spinbox
+        # Methods to set/get tkinter variable associated with DutyCycle spinbox
         # --------------
         def set_DCYC_var(self,var):
                 self.__DCYC_var = var
@@ -160,17 +124,16 @@ class PIN:                                                              # Define
                 return self.__DCYC_var
 
         # --------------
-        # Method to set tkinter variable associated with edge widget
+        # Methods to set/get tkinter variable associated with edge widget
         # --------------
         def set_EDGE_var(self,var):
                 self.__EDGE_var = var
 
         def get_EDGE_var(self):
-                return self.__EDGE_var
-        
+                return self.__EDGE_var      
 
         # --------------
-        # Method to set tkinter variable associated with bounce widget
+        # Methods to set/get tkinter variable associated with bounce widget
         # --------------
         def set_BOUNCE_var(self,var):
                 self.__BOUNCE_var = var
@@ -179,50 +142,124 @@ class PIN:                                                              # Define
                 return self.__BOUNCE_var
 
         # --------------
-        # Method to set output pin to high or low
+        # Methods to set/get references to GUI widgets
         # --------------
-        def set_output(self,val):              # Pass boolean vaue to indicate whether to turn on (True) or off (False)
-                if self.get_direction()=="OUT": #self.OUTPUT:   # Only if pin is configured for output
-                        self.__status = val             # Set status to val
-                        GPIO.output(self.__id,val)      # Set pin to high/low
-                else:
-                        print("PIN is not set for output")
+        def set_OM_DIR(self,widget):
+                self.__OM_dir = widget
 
-        # --------------
-        # Method to self describe instanxce info...called by print function
-        # --------------
-        def __str__(self):
-                return "PIN ID: " + str(self.__id) + "\tDirection: " + str(self.__direction) +  "\tStatus: " + str(self.__status)
+        def get_OM_DIR(self):
+                return self.__OM_DIR
 
-        # --------------
-        # Method to store references to GUI widgets
-        # --------------
-        def set_OH_CBX(self,widget):
-                self.__OH_cBox = widget
+        def set_CB_OH(self,widget):
+                self.__CB_outH = widget
 
-        def set_OM_dir(self,widget):
-                self.__OM_DIR = widget
-                
-        def set_PWM_CBX(self,widget):
+        def get_CB_OH(self):
+                return self.__CB_outH
+
+        def set_CB_PWM(self,widget):
                 self.__PWM_cBox = widget
+
+        def get_CB_PWM(self):
+                return __PWM_cBox
 
         def set_SB_FREQ(self,widget):
                 self.__SB_freq = widget
 
+        def get_SB_FREQ(self):
+                return __SB_freq
+
         def set_SB_DCYC(self,widget):
                 self.__SB_dcyc = widget
 
+        def get_SB_DCYC(self):
+                return __SB_dcyc
+        
         def set_OM_EDGE(self,widget):
                 self.__OM_edge = widget
 
+        def get_OM_EDGE(self):
+                return self.__OM_edge
+        
         def set_OM_BOUNCE(self,widget):
                 self.__OM_bounce = widget
+
+        def get_OM_BOUNCE(self):
+                return __OM_bounce
+        
+        # --------------
+        # Method to set direction (input or output) of pin
+        # --------------
+        def set_direction(self, dir):
+                if dir == PIN.INPUT_TXT:                        # If input
+                        self.__direction = dir                  # Set __direction variable
+                        if not PIN.NO_GPIO:
+                                GPIO.setup(self.get_id(),GPIO.IN) # Set pin for input
+                        if self.get_CB_OH() != self.UNDEFINED:    # If gui chkbox exists, disable chkbox
+                                self.__CB_outH.deselect()
+                                self.__CB_outH.config(state="disabled")
+                                self.unset_eventDet()
+                                #
+                                # Need to add logic to change OM_EDGE to NONE
+                                #
+                elif dir == PIN.OUTPUT_TXT:                     # if output
+                        self.__direction = dir                  # Set __direction variable
+                        try:
+                                if self.__event_det == PIN.ED_ON:
+                                        self.__event_det = PIN.ED_OFF
+                                        if not PIN.NO_GPIO:
+                                                GPIO.remove_event_detect(self.get_id())        
+                        except Exception as err:
+                                print("***",end=" ")
+                                print(err)
+                        if not PIN.NO_GPIO:
+                                GPIO.setup(self.get_id(),GPIO.OUT)          # Set pin for output
+                        if self.__CB_outH != self.UNDEFINED:     # If gui chkbox exists... 
+                                self.__CB_outH.config(state="normal") #  enable chkbox
+                                if self.get_status():
+                                        self.__CB_outH.select()
+                                else:
+                                        self.__CB_outH.deselect()
+                else:
+                        print("Error ...shold not get here.")
+
+        # --------------
+        # Methods to add/remove event detection for a pin
+        # --------------
+        def set_eventDet(self, edge):          
+                if (self.__id != 19) and (self.__id != 21) :  # pin 19 and 21 seem to constantly switch state
+                        if not PIN.NO_GPIO:
+                              GPIO.add_event_detect(self.__id,edge,callback=self.callback_pin_state_change,bouncetime=200)
+                        self.__event_det = PIN.ED_ON
+
+        def unset_eventDet(self):
+                GPIO.remove_event_detect(self.get_id())
                 
+
+
+        # --------------
+        # Method to set output pin to high or low
+        # --------------
+        def set_output(self,val):              # Pass boolean vaue to indicate whether to turn on (True) or off (False)
+                if self.get_direction() == PIN.OUTPUT_TXT:  # Only if pin is configured for output
+                        self.__status = val             # Set status to val
+                        if not PIN.NO_GPIO:
+                                GPIO.output(self.__id,val)      # Set pin to high/low
+                else:
+                        print("PIN is not set for output")
+
+        # --------------
+        # Method to self describe instance info...called by print function
+        # --------------
+        def __str__(self):
+                return "PIN ID: " + str(self.__id) + "\tDirection: " + str(self.__direction) +  "\tStatus: " + str(self.__status)
+
+        
         # --------------
         # Method called when a direction oMenu option is selected
         # --------------
         def dir_selected(self,dir):
-                selected_mode = dir # self.__DIR_var.get()
+                selected_mode = dir
+                print("Direction selected:", dir)
                 if selected_mode == self.get_direction():
                         print("Button already selected")
                 else:
@@ -248,8 +285,9 @@ class PIN:                                                              # Define
                         self.__SB_dcyc.config(state='normal')
                                
                         if self.get_direction() == self.OUTPUT_TXT:
-                                self.__pwm = GPIO.PWM(self.__id,50)
-                                self.__pwm.start(50)                            
+                                if not PIN.NO_GPIO:
+                                        self.__pwm = GPIO.PWM(self.__id,50)
+                                        self.__pwm.start(50)                            
                 else:
                         print(" Pin: ",self.get_id(),"turn PWM off (not implement yet)")
                         self.__SB_freq.config(state='disable')
@@ -286,13 +324,11 @@ class PIN:                                                              # Define
         # --------------
         def callback_pin_state_change(self,channel):
                 print(" Pin ",self.get_id()," has changed")
-                if GPIO.input(self.__id)==self.HIGH:
-                        print("Pin",self.__id," is HIGH")
-                        
-                        #self.__RB_inp.configure(fg='red')
-                else:
-                        #self.__RB_inp.configure(fg='black')
-                        print("Pin",self.__id," is LOW")
+                if not PIN.NO_GPIO:
+                        if GPIO.input(self.__id)==self.HIGH:
+                                print("Pin",self.__id," is HIGH")
+                        else:
+                                print("Pin",self.__id," is LOW")
                 
  
 # End of PIN class
--- gpio_dashboard1.py
+++ gpio_dashboard1.py
@@ -8,6 +8,7 @@ import  MY_GPIO_class   as MYGPIO
 # Constant definitions
 # --------------
 
+NO_GPIO         = False # Set to True to test program GUI with no GPIO calls
 NUM_PINS        = 26    # Number of pins to be shown in dashboard
 P_NM_COL_W      = 10    # Pin name column width
 P_RB_COL_W      = 4     # Radiobutton column width
@@ -24,7 +25,7 @@ EDGE_COL        = 8
 BOUNCE_COL      = 9
 
 DIR_MENU_OPTS   = ["IN","OUT"]
-EDGE_OPTS       = ["FALLING","RISING","BOTH"]
+EDGE_OPTS       = ["NONE","FALLING","RISING","BOTH"]
 BOUNCE_OPTS     = ["0","100","200"]
 
 I2C_COLOR       = 'blue'
@@ -34,31 +35,31 @@ UART_COLOR      = 'yellow'
 
 DB_WINDOW_TITLE = "Bart's GPIO Dashboard"
 
-PIN_LIST        = [[2,'I2C'], \
-                  [3,'I2C'], \
-                  [4,''], \
-                  [5,''], \
-                  [6,''], \
-                  [7,'SPI'], \
-                  [8,'SPI'], \
-                  [9,'SPI'], \
-                  [10,'SPI'], \
-                  [11,'SPI'], \
-                  [12,''], \
-                  [13,''], \
-                  [14,'UART'], \
-                  [15,'UART'], \
-                  [16,''], \
-                  [17,''], \
-                  [18,'PCM'], \
-                  [19,'PCM'], \
-                  [20,'PCM'], \
-                  [21,'PCM'], \
-                  [22,''], \
-                  [23,''], \
-                  [24,''], \
-                  [25,''], \
-                  [26,''], \
+PIN_LIST        = [[2,'I2C'],   \
+                  [3,'I2C'],    \
+                  [4,''],       \
+                  [5,''],       \
+                  [6,''],       \
+                  [7,'SPI'],    \
+                  [8,'SPI'],    \
+                  [9,'SPI'],    \
+                  [10,'SPI'],   \
+                  [11,'SPI'],   \
+                  [12,''],      \
+                  [13,''],      \
+                  [14,'UART'],  \
+                  [15,'UART'],  \
+                  [16,''],      \
+                  [17,''],      \
+                  [18,'PCM'],   \
+                  [19,'PCM'],   \
+                  [20,'PCM'],   \
+                  [21,'PCM'],   \
+                  [22,''],      \
+                  [23,''],      \
+                  [24,''],      \
+                  [25,''],      \
+                  [26,''],      \
                   [27,'']]
 
 # --------------
@@ -66,30 +67,33 @@ PIN_LIST        = [[2,'I2C'], \
 #---------------
 
 def create_DB_Header():
-        root.title(DB_WINDOW_TITLE)
+        master.title(DB_WINDOW_TITLE)
 
         # Add labels to header frame
         tk.Label(root,text = "PIN ID",  fg='blue',padx=COL_PAD,width=P_NM_COL_W).grid(row=0,column=P_NM_COL)
-        tk.Label(root,text = " ",       fg='blue',padx=COL_PAD,width=3).grid(row=0,column=1)
+        tk.Label(root,text = " ",       fg='blue',padx=COL_PAD,width=COL_PAD).grid(row=0,column=1)
         tk.Label(root,text = "IN/OUT",  fg='blue',padx=COL_PAD,width=P_RB_COL_W).grid(row=0,column=DIR_IO_COL)
         tk.Label(root,text = "HIGH",    fg='blue',padx=COL_PAD,width=P_RB_COL_W).grid(row=0,column=OUT_HIGH_COL)
         tk.Label(root,text = "PWM",     fg='blue',padx=COL_PAD,width=P_RB_COL_W).grid(row=0,column=PWM_COL)
         tk.Label(root,text = "FREQ",    fg='blue',padx=COL_PAD,width=P_RB_COL_W).grid(row=0,column=FREQ_COL)
         tk.Label(root,text = "D_CYC",   fg='blue',padx=COL_PAD,width=P_RB_COL_W).grid(row=0,column=D_CYC_COL)
-        tk.Label(root,text = " ",       fg='blue',padx=COL_PAD,width=3).grid(row=0,column=7)
+        tk.Label(root,text = " ",       fg='blue',padx=COL_PAD,width=COL_PAD).grid(row=0,column=7)
         tk.Label(root,text = "EDGE",    fg='blue',padx=COL_PAD,width=10).grid(row=0,column=EDGE_COL)
         tk.Label(root,text = "BOUNCE",  fg='blue',padx=COL_PAD,width=10).grid(row=0,column=BOUNCE_COL)
 
+# --------------
+# Function to create vars used to transfer widget changes and set pointer in PIN objects
+#---------------
+
 def set_widget_vars(pin):
         vDir = tk.StringVar()                   # Variable for pin direction
-        vDir.set(DIR_MENU_OPTS[1])              # Set to Output by default
         pin.set_DIR_var(vDir)
 
         vCB_out = tk.IntVar(pin.LOW)            # Create variable for output-high button
-        pin.set_OUT_var(vCB_out)                # Set pin to LOW
+        pin.set_OUT_var(vCB_out)                
 
         vCB_pwm = tk.IntVar(pin.PWM_OFF)        # Create variable for PWM chkbox
-        pin.set_PWM_var(vCB_pwm)                # Set pin to LOW
+        pin.set_PWM_var(vCB_pwm)                
 
         vSB_freq = tk.StringVar("")             # Frequency 
         pin.set_FREQ_var(vSB_freq)           
@@ -98,21 +102,23 @@ def set_widget_vars(pin):
         pin.set_DCYC_var(vSB_dcyc)           
 
         vEdge = tk.StringVar("")                # Edge (Falling, rising, or both)
-        vEdge.set(EDGE_OPTS[0])
         pin.set_EDGE_var(vEdge)
 
         vBounce = tk.StringVar("")              # Bounce value
-        vBounce.set(BOUNCE_OPTS[0])
         pin.set_BOUNCE_var(vBounce)
         
 # --------------
 # Main line of program
 #---------------
 
-gpio = MYGPIO.myGPIO()  # My class to initialize GPIO
+if not NO_GPIO:
+        gpio = MYGPIO.myGPIO()  # My class to initialize GPIO
 
-root = tk.Tk()          # Initialize gui
-
+master = tk.Tk()          # Initialize gui
+root = Canvas(master,width=600,height=100)
+root.pack()
+#vsb = Scrollbar(root,orient="vertical")
+#vsb.pack(side="right")
 create_DB_Header()
 
 # Initialize pins
@@ -120,29 +126,29 @@ for p in PIN_LIST:
         pin_num=p[0]
         pin = PIN.PIN(pin_num)  # Create pin object
 
-#       vDir = tk.StringVar()
-#        vDir.set(DIR_MENU_OPTS[0])
-#        pin.set_DIR_var(vDir)#
-#
-#        vCB_out = tk.IntVar(pin.LOW)    # Create variable for output-high button
-#        pin.set_OUT_var(vCB_out)             # Set pin to LOW#
-#
-#        vCB_pwm = tk.IntVar(pin.PWM_OFF)              # Create variable for PWM chkbox
-#        pin.set_PWM_var(vCB_pwm)           # Set pin to LOW
-#
-#        vSB_freq = tk.StringVar("")              
-#        pin.set_FREQ_var(vSB_freq)           
-#
-#        vSB_dcyc = tk.StringVar("")              
-#        pin.set_DCYC_var(vSB_dcyc)           
-#
-#        vEdge = tk.StringVar("")
+        vDir = tk.StringVar()
+        vDir.set(DIR_MENU_OPTS[1])
+        pin.set_DIR_var(vDir) 
+
+        vCB_out = tk.IntVar(pin.LOW)    # Create variable for output-high button
+        pin.set_OUT_var(vCB_out)        # Set pin to LOW#
+
+        vCB_pwm = tk.IntVar(pin.PWM_OFF)# Create variable for PWM chkbox
+        pin.set_PWM_var(vCB_pwm)        # Set pin to LOW
+
+        vSB_freq = tk.StringVar("")              
+        pin.set_FREQ_var(vSB_freq)           
+
+        vSB_dcyc = tk.StringVar("")              
+        pin.set_DCYC_var(vSB_dcyc)           
+
+        vEdge = tk.StringVar("")
 #        vEdge.set(EDGE_OPTS[0])
-#        pin.set_EDGE_var(vEdge)
+        pin.set_EDGE_var(vEdge)
 #
-#        vBounce = tk.StringVar("")
+        vBounce = tk.StringVar("")
 #        vBounce.set(BOUNCE_OPTS[0])
-#        pin.set_BOUNCE_var(vBounce)
+        pin.set_BOUNCE_var(vBounce)
         
         
         # Add lbl(pin name) and other controls for each button
@@ -159,10 +165,10 @@ for p in PIN_LIST:
                 pass
         
 
-# Add colunm for In/Out selection 
+# Add colunm for In/Out selection
         oMenu_dir   = tk.OptionMenu(root,pin.get_DIR_var(),*DIR_MENU_OPTS,command = pin.dir_selected)
         oMenu_dir.config(width=5)
-
+       
 # Add column for forcing output to high
         cbox_high   = tk.Checkbutton(root,padx=COL_PAD,width=P_RB_COL_W,variable=pin.get_OUT_var(),onvalue=1,offvalue=0,command=pin.out_high_selected)
 
@@ -204,8 +210,8 @@ for p in PIN_LIST:
 
         if pin.get_direction() != pin.OUTPUT_TXT:# If direction is not output, 
                 cbox_high.config(state=DISABLED)        # disable checkbox
-        pin.set_OH_CBX(cbox_high)                      # Store linkto checkbox object in pin instance
-        pin.set_PWM_CBX(cbox_pwm)
+        pin.set_CB_OH(cbox_high)                      # Store linkto checkbox object in pin instance
+        pin.set_CB_PWM(cbox_pwm)
         pin.set_SB_FREQ(sbox_freq)
         pin.set_SB_DCYC(sbox_dcyc)
         pin.set_OM_EDGE(oMenu_edge)
@@ -213,6 +219,7 @@ for p in PIN_LIST:
         pin.set_OM_BOUNCE(scale_bounce) 
 root.mainloop()
 
-gpio.cleanup()
+if not NO_GPIO:
+        gpio.cleanup()
 
 # End program
